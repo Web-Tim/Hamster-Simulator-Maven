323d5992727877688b5a2f9572681dceecf66726
Web-Tim <teichmanntim@outlook.de>
2022-11-24 12:54:59 +0100

    implement Terrain loading

--- src/main/java/me/tim/Bootstrap.java
+++ src/main/java/me/tim/Bootstrap.java
@@ -1,7 +1,10 @@
 package me.tim;
 
 import hamster.Hamster;
+import hamster.Territorium;
 import me.tim.app.impl.TimAlgorithm;
+
+import java.io.File;
 
 /**
  * Start-Klasse des Hamsters.
@@ -10,6 +13,14 @@ import me.tim.app.impl.TimAlgorithm;
  * @since 24.11.2022
  */
 public class Bootstrap extends Hamster {
+    static {
+        File f = new File("Territorien/default.ter");
+        if (!f.exists()) {
+            System.err.println("Default Terrain couldn't be loaded!");
+        }
+        Territorium.ladeTerritorium(f.getPath());
+    }
+
     public Bootstrap() {
         super(1, 1, 1, 1);
         new TimAlgorithm(this).run();
